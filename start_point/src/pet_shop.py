afeb7e9b3ff4957cbe6e7f20ca76e320611a1e0e
Lewis <lewisdsykes@gmail.com>
2022-06-24 13:14:33 +0100

    First commit for weekend homework

---
+++ start_point/src/pet_shop.py
@@ -0,0 +1,24 @@
+# WRITE YOUR FUNCTIONS HERE
+
+def get_pet_shop_name(name):
+    return name["name"]
+
+def get_total_cash(cash):
+    return cash["admin"]["total_cash"]
+
+# def add_or_remove_cash(cash):
+#     t_cash = total_cash + cash
+
+    
+
+def get_pets_sold(sold):
+    return sold["admin"]["pets_sold"]
+
+def increase_pets_sold(sold):
+    total_
+
+
+
+
+
+
